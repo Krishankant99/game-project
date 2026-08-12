1a68ff356abee71187fd91261ce117ebf92c283c
Krishankant Chaurasia <krishankantchaurasia1998@gmail.com>
2026-08-13 00:56:25 +0530

    Initialize Express app with EJS and static files

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`🎮 Math Battle Game running on port ${PORT}`);
+});
